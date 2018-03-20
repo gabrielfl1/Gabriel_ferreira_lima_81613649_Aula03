c2c14908c76cacf8ac1c1a784b09d701b014dfed
arqdsis <arqdsis@mocli10211.animaedu.intranet>
2018-03-20 18:27:29 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/desmob/Aula03/PaisesContract.java
@@ -0,0 +1,21 @@
+package br.usjt.desmob.Aula03;
+
+import android.provider.BaseColumns;
+
+/**
+ * Gabriel Ferreira Lima - 81613649
+ */
+
+public class PaisesContract {
+
+    public static abstract class PaisEntry implements BaseColumns{
+        public static final String TABLE_NAME = "pais";
+        public static final String COLUMN_NAME_NOME = "nome";
+        public static final String COLUMN_NAME_REGIAO = "regiao";
+        public static final String COLUMN_NAME_CAPITAL = "capital";
+        public static final String COLUMN_NAME_BANDEIRA = "bandeira";
+        public static final String COLUMN_NAME_CODIGO3 = "codigo3";
+
+
+    }
+}
